91e0cdd3d98440d7c6d695f5b7c3f05753dabc16
Torsten Curdt <tcurdt@vafer.org>
2008-12-23 23:42:41 +0100

    testapp skeleton

---
+++ Test/TestApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TestApp
+//
+//  Created by Torsten Curdt on 23.12.08.
+//  Copyright vafer.org 2008. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
